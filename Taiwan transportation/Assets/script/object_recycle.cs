9e685fb9fdfe49086e069957166e3fa54e6ce508
黃少辰 <92316534+FungiSan@users.noreply.github.com>
2023-03-04 22:35:35 +0800

    added PlayerUI and modified recycle

--- Taiwan transportation/Assets/script/object_recycle.cs
+++ Taiwan transportation/Assets/script/object_recycle.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class object_recycle : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other) 
+    private void Update() {
+        if(hitBorder())
+            gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag=="border"||other.gameObject.tag=="enemy")
+        if(other.gameObject.tag=="enemy")
         {
             gameObject.SetActive(false);
         }
+    }
+
+    private bool hitBorder(){
+        return this.transform.position.x <-8.7 || this.transform.position.x > 3 || 
+                this.transform.position.y > 7 || this.transform.position.y < -7 ;
     }
 }
