d8d47fd362b7b7d1a067a12b74d893d73968ffba
黃少辰 <92316534+FungiSan@users.noreply.github.com>
2023-06-29 14:31:22 +0800

    Stage 1 and Model Moving

--- Taiwan transportation/Assets/script/Enemy/EnemyManager.cs
+++ Taiwan transportation/Assets/script/Enemy/EnemyManager.cs
@@ -7,45 +7,31 @@ public class EnemyManager : MonoBehaviour
     [SerializeField]float summonTime;
     [SerializeField]GameObject[] enemy;
     
-    Vector2 moveVector;
+    Vector2 speedVector;
+    Vector3 enemySpawnPos;
     int enemyType;
     const float RIGHTBORDER = 6.2f;
     const float LEFTBORDER = -6.2f;
     const float UPPERBORDER = 7.2f;
     const float LOWERBORDER = -7.2f;
-    void Start(){
-        StartCoroutine(stageStart());
+
+    IEnumerator startStage(){
+
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(summonMonster());
     }
 
-    IEnumerator stageStart(){
-        Vector3 tmp_spawnPos;
-        
-        for(int i=0; i<5; i++){
-            tmp_spawnPos = new Vector3(RIGHTBORDER, 6f - i, 0f);
-            moveVector = new Vector2(-1f, 0f);
-            spawnEnemy(1, tmp_spawnPos, 1, 1);
-            
-        }
-        yield return new WaitForSeconds(3f);
-
-        for(int i=0; i<6; i++){
-            tmp_spawnPos = new Vector3(LEFTBORDER + 1.5f*i, 6.5f, 0f);
-            moveVector = new Vector2(1f, 0f);
-            spawnEnemy(0, tmp_spawnPos, 2, 1);
-            
-            yield return new WaitForSeconds(0.5f);
-        }
-
-        
-        
+    IEnumerator summonMonster(){
+        yield return new WaitForSeconds(1f);
+               
     }
 
-    void spawnEnemy(int ememyType, Vector3 spawnPos, int moveType, int shootType){
-        GameObject tmpEnemy = Instantiate(enemy[enemyType], spawnPos, transform.rotation);
+    void spawnEnemy(int ememyType, int moveType, int shootType){
+        GameObject tmpEnemy = Instantiate(enemy[enemyType], enemySpawnPos, transform.rotation);
         tmpEnemy.GetComponent<EnemyBehavior>().moveType = moveType;
         tmpEnemy.GetComponent<EnemyBehavior>().shootType = shootType;
-        if(moveType == 1)
-            tmpEnemy.GetComponent<EnemyBehavior>().moveVector = moveVector;
+        tmpEnemy.GetComponent<EnemyBehavior>().speedVector = speedVector;
         
     }
 }
---
+++ Taiwan transportation/Assets/script/Stages/Stage1.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage1 : MonoBehaviour
+{
+    [SerializeField]float summonTime;
+    [SerializeField]GameObject[] enemies;
+    
+    Vector2 speedVector;
+    int accelType;
+    int shoot_angle_diff;
+    Vector2 accelVector;
+    
+    const float RIGHTBORDER = 6.2f;
+    const float LEFTBORDER = -6.2f;
+    const float UPPERBORDER = 7.2f;
+    const float LOWERBORDER = -7.2f;
+    public string next_stage = "Stage_2";
+    
+    void Start(){
+        StartCoroutine(startStage());
+        if(Input.GetKey(KeyCode.N)){
+            SceneManager.LoadScene(next_stage);
+        }
+    }
+
+    IEnumerator startStage(){
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(summonMonster());
+    }
+
+    IEnumerator summonMonster(){
+        GameObject enemy;
+        enemy = Instantiate(enemies[0], new Vector2(-3f, 5f), transform.rotation);
+        enemy.GetComponent<ModelMovement>().moveList = new List<ModelMovement.fumoType>();
+        enemy.GetComponent<ModelMovement>().moveList.Add(new ModelMovement.fumoType(1, new Vector2(0, -2), 1));
+
+        enemy.GetComponent<ModelMovement>().accelList = new List<ModelMovement.fumoType>();
+        enemy.GetComponent<ModelMovement>().accelList.Add(new ModelMovement.fumoType(0, new Vector2(0.1f, 0), 1f));
+        enemy.GetComponent<ModelMovement>().accelList.Add(new ModelMovement.fumoType(1, new Vector2(0.1f, 0), 3f));
+        
+        yield return new WaitForSeconds(0.5f);
+        /*
+        for(int i=0; i<5; i++){
+            speedVector = new Vector2(0f, -2f);
+            
+            enemy = spawnEnemy(0, 1, 1, new Vector3(3.5f, 7.2f, 0f));
+            modifyEnemyAccel(enemy, 1, new Vector2(-0.1f - i*.01f, 0f));
+            enemy.GetComponent<EnemyBehavior>().shoot_angle_diff = 10;
+
+            enemy = spawnEnemy(0, 1, 1, new Vector3(4.5f, 7.2f, 0f));
+            modifyEnemyAccel(enemy, 1, new Vector2(-0.1f - i*.01f, 0f));
+            enemy.GetComponent<EnemyBehavior>().shoot_angle_diff = 10;
+
+            yield return new WaitForSeconds(0.5f);
+        }
+        
+        for(int i=0; i<5; i++){
+            speedVector = new Vector2(0f, -2f);
+
+            enemy = spawnEnemy(0, 1, 1, new Vector3(-3.5f, 7.2f, 0f));
+            modifyEnemyAccel(enemy, 1, new Vector2(0.1f + i*.01f, 0f));
+            enemy.GetComponent<EnemyBehavior>().shoot_angle_diff = 10;
+
+            enemy = spawnEnemy(0, 1, 1, new Vector3(-4.5f, 7.2f, 0f));
+            modifyEnemyAccel(enemy, 1, new Vector2(0.1f + i*.01f, 0f));
+            enemy.GetComponent<EnemyBehavior>().shoot_angle_diff = 10;
+
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        for(int i=-5; i<=5; i++){
+            speedVector = new Vector2(0f, -2f);
+            spawnEnemy(0, 1, 1, new Vector3(-i*0.8f, 7.2f, 0f));
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        for(int i=-4; i<=4; i++){
+            speedVector = new Vector2(0f, -2f);
+            spawnEnemy(0, 1, 2, new Vector3(i, 7.2f, 0f));
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        for(int i=-5; i<=5; i++){
+            speedVector = new Vector2(0f, -2f);
+            spawnEnemy(0, 1, 1, new Vector3(-i*0.8f, 7.2f, 0f));
+            yield return new WaitForSeconds(0.05f);
+        }*/
+
+    }
+    void modifyEnemyAccel(GameObject enemy, int accelType, Vector2 accelVector){
+        enemy.GetComponent<EnemyBehavior>().accelType = 1;
+        enemy.GetComponent<EnemyBehavior>().accelVector = accelVector;
+    }
+    GameObject spawnEnemy(int enemyType, int moveType, int shootType, Vector3 enemySpawnPos){
+        GameObject tmpEnemy = Instantiate(enemies[enemyType], enemySpawnPos, transform.rotation);
+        tmpEnemy.GetComponent<EnemyBehavior>().moveType = moveType;
+        tmpEnemy.GetComponent<EnemyBehavior>().shootType = shootType;
+        tmpEnemy.GetComponent<EnemyBehavior>().speedVector = speedVector;
+        
+        return tmpEnemy;
+    }
+}
