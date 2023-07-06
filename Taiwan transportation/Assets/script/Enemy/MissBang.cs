e801fd307f3a3b3c0aff8edcbabfe6a773655770
gary <gary313128@gmail.com>
2023-07-06 14:04:24 +0800

    simple state machine

--- Taiwan transportation/Assets/script/Enemy/MissBang.cs
+++ Taiwan transportation/Assets/script/Enemy/MissBang.cs
@@ -11,6 +11,10 @@ public class MissBang : MonoBehaviour
     objectPooler obj_instance;
     ModelMovement MM;
     Clock clock;
+    int HP=100,limitHP=20,section=0;
+    bool actionChecker=false;
+    public enum Section {setPos,normalAtk_1,glassRain,normalAtk_2,brockenCar,end}
+    bool[] sectionCheck = new bool[6]; //check whether a section is actived
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
@@ -18,29 +22,66 @@ public class MissBang : MonoBehaviour
         MM = GetComponent<ModelMovement>();
         obj_instance = objectPooler.instance;
         clock = Clock.clockInstance;
-        StartCoroutine(brockenCar());
+        for(int i=0;i<6;i++)sectionCheck[i] = false;
+        section=0;
+        actived();
     }
     void Update()
     {
         an.SetFloat("x_velocity",rb.velocity.x);
+        if(actionChecker)
+        {
+            if(section==0 && !sectionCheck[(int)Section.setPos]) setPos();
+            if(section==1 && !sectionCheck[(int)Section.normalAtk_1])
+            {
+                Debug.Log("1");
+                StartCoroutine(normalAtk_1());
+            }
+
+            if(section==2 && !sectionCheck[(int)Section.glassRain])
+            {
+                Debug.Log("2");
+                StartCoroutine(glassRain());
+            }
+            
+            if(section==3 && !sectionCheck[(int)Section.normalAtk_2])
+            {
+                StartCoroutine(normalAtk_2());
+            }
+
+            if(section==4 && !sectionCheck[(int)Section.brockenCar])
+            {
+                StartCoroutine(brockenCar());
+            }
+
+            if(section == (int)Section.end)actionChecker = false;
+        }
     }
-    IEnumerator normalShoot_1()
+    void setPos() // not complete yet
     {
+        sectionCheck[(int)Section.setPos] = true;
+        transform.position = new Vector2(0,5);
+        section++;
+    }
+    IEnumerator normalAtk_1()
+    {
+        sectionCheck[(int)Section.normalAtk_1] = true;
         GameObject[] shooter = new GameObject[3];
         GameObject clone,player = GameObject.FindGameObjectWithTag("Player");
-        int layer=0,bulletEachCol=5;
+        int layer=0,bulletEachCol=10;
         for(int i=0;i<3;i++) shooter[i] = transform.GetChild(i).gameObject;
         shooter[0].transform.localPosition = new Vector2(0,-0.5f);
-        shooter[1].transform.localPosition = new Vector2(-0.5f,-0.5f);
-        shooter[2].transform.localPosition = new Vector2(0.5f,-0.5f);
+        shooter[1].transform.localPosition = new Vector2(-0.25f,-0.5f);
+        shooter[2].transform.localPosition = new Vector2(0.25f,-0.5f);
 
-        clock.setTimer("timer",10f);
-        while(clock.checkTimer("timer"))
+        clock.setTimer("timer",5f);
+        while(clock.checkTimer("timer") && HP>limitHP)
         {
+            yield return new WaitForSeconds(1f);
             Vector2 target = player.transform.position;
             for(int i=0;i<bulletEachCol;i++)
             {
-                yield return new WaitForSeconds(0.05f);
+                yield return new WaitForSeconds(0.1f);
                 for(int j=0;j<3;j++)
                 {
                     Vector2 dire = target-(Vector2)shooter[j].transform.position;
@@ -51,10 +92,13 @@ public class MissBang : MonoBehaviour
                     layer++;
                 }
             }
-        } 
+        }
+        section++;
+        Debug.Log("too fast");
     }
-    IEnumerator normalShoot_2()
+    IEnumerator normalAtk_2()
     {
+        sectionCheck[(int)Section.normalAtk_2] = true;
         GameObject shooter = transform.GetChild(0).gameObject;
         GameObject colone;
         Vector2 dire = new Vector2(1,0);
@@ -75,9 +119,11 @@ public class MissBang : MonoBehaviour
                 layer++;
             }
         }
+        section++;
     }
     IEnumerator glassRain() 
     {
+        sectionCheck[(int)Section.glassRain] = true;
         GameObject shooter = transform.GetChild(0).gameObject;
         GameObject colone;
         shooter.transform.localPosition = new Vector3(0,0,0);
@@ -87,41 +133,60 @@ public class MissBang : MonoBehaviour
         {
             Vector2 dire = new Vector2(1,0)*10;
             colone = Instantiate(backMirror,shooter.transform.position,shooter.transform.rotation);
-            ModelMovement MM = colone.GetComponent<ModelMovement>();
             int chose = Random.Range(1,3);
             if(chose==1) dire = ourTool.trans_matrix(dire,ourTool.eulerToRadian(Random.Range(30,80)));
             else if(chose==2) dire = ourTool.trans_matrix(dire,ourTool.eulerToRadian(Random.Range(100,150)));
-            MM.setMovement(1,dire,1);
-            MM.startAll();
+            colone.GetComponent<Rigidbody2D>().velocity = dire;
             yield return new WaitForSeconds(2);
         }
+        section++;
     }
     IEnumerator brockenCar()
     {
+        sectionCheck[(int)Section.brockenCar] = true;
         GameObject [] shooter = new GameObject[3];
         GameObject clone;
-        int bulletEachCircle=120,layer=0;
+        int bulletEachCircle=100,layer=0;
 
         for(int i=0;i<3;i++)
         {
             shooter[i] = transform.GetChild(i).gameObject;
             shooter[i].transform.localPosition = new Vector2(0,0);
         }
-        for(int i=0;i<3;i++)
+        for(int k=0;k<5;k++)
         {
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(1f);
             Vector2 dire = new Vector2(0,-1);
-            for(int j=0;j<bulletEachCircle;j++)
+            int oritation = Random.Range(-2,2);
+            for(int i=0;i<3;i++)
             {
-                shooter[i].transform.Rotate(new Vector3(0,0,360/bulletEachCircle));
-                clone = obj_instance.spawnFromPool("aqua_bullet",shooter[i].transform.position,shooter[i].transform.rotation,null);
-                clone.GetComponent<SpriteRenderer>().sortingOrder = layer;
-                clone.GetComponent<Rigidbody2D>().velocity = dire*5;
-
-                dire = ourTool.trans_matrix(dire,ourTool.eulerToRadian(360/bulletEachCircle));
-                layer++;
+                yield return new WaitForSeconds(0.2f);
+                for(int j=0;j<bulletEachCircle;j++)
+                {
+                    if(j>1)
+                    {
+                        shooter[i].transform.Rotate(new Vector3(0,0,360/bulletEachCircle));
+                        clone = obj_instance.spawnFromPool("aqua_bullet",shooter[i].transform.position,shooter[i].transform.rotation,null);
+                        clone.GetComponent<SpriteRenderer>().sortingOrder = layer;
+                        clone.GetComponent<Rigidbody2D>().velocity = dire*3;
+                    }
+                    dire = ourTool.trans_matrix(dire,ourTool.eulerToRadian(360f/bulletEachCircle));
+                    layer++;
+                }
+                if(oritation>0)
+                {
+                    dire = ourTool.trans_matrix(dire,ourTool.eulerToRadian(5));
+                }
+                else 
+                {
+                    dire = ourTool.trans_matrix(dire,ourTool.eulerToRadian(-5));
+                }
             }
-            
         }
+        section++;
+    }
+    public void actived()
+    {
+        actionChecker = true;
     }
 }
