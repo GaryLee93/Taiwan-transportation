70f61d41c1c7e1f3287ea5c6670b7c2c5ddb115c
黃少辰 <92316534+FungiSan@users.noreply.github.com>
2023-08-11 18:01:52 +0800

    add 小翠兒spin

--- Taiwan transportation/Assets/Scripts/Emeny_bullet/backMirrorBehavior.cs
+++ Taiwan transportation/Assets/Scripts/Emeny_bullet/backMirrorBehavior.cs
@@ -33,7 +33,7 @@ public class backMirrorBehavior : MonoBehaviour
             if(bounce)
             {
                 
-                transform.Rotate(new Vector3(0f, 0f, Random.Range(0f, 180f)));
+                transform.Rotate(new Vector3(0f, 0f, Random.Range(60f, 120f)));
                 rb.velocity = new Vector2(new_x,new_y);
                 GameObject shooter = transform.GetChild(0).gameObject;
                 GameObject colone;
